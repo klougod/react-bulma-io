39cb68b47a2e853eea8b41324a96e7eff86a6aad
klougod <brunoomagalhaes98@gmail.com>
2019-10-22 19:22:10 -0300

    added content component

---
+++ src/lib/components/Content.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Content = ({ className, children }) => (
+  <div className={`content ${className}`}>
+    { children }
+  </div>
+)
+
+Content.defaultProps = {
+  className: ''
+}
+
+Content.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+}
+
+export default Content
