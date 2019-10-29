d746c58832f8f7fe6766bc2f77561100d24a855c
klougod <brunoomagalhaes98@gmail.com>
2019-10-29 13:29:23 -0300

    added checkbox component

---
+++ src/lib/components/Checkbox.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Checkbox = ({ text, checked, name, className, onChange }) => {
+
+  return (
+    <label class="checkbox">
+      <input type="checkbox" className={className} checked={checked} onChange={onChange} name={name} />
+      &nbsp;{ text }
+    </label>
+  )
+}
+
+Checkbox.defaultProps = {
+  checked: false,
+  onChange: null,
+  name: '',
+  text: '',
+  className: ''
+}
+
+Checkbox.propTypes = {
+  checked: PropTypes.bool,
+  onChange: PropTypes.func,
+  name: PropTypes.string,
+  text: PropTypes.string,
+  className: PropTypes.string
+}
+
+export default Checkbox;
