4a0c50b10efca48dd91ce6a6899d252422f29870
klougod <brunoomagalhaes98@gmail.com>
2019-10-29 13:29:11 -0300

    added input and checkbox components

--- src/lib/components/Input.js
+++ src/lib/components/Input.js
@@ -2,9 +2,8 @@ import React, { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Swal from 'sweetalert2'
 
-export const Input = ({ value, label, icon, type, error, name, customInput, placeholder, isRequired, className, onChange, tipMessage }) => {
+export const Input = ({ value, label, icon, type, error, name, customInput, placeholder, isRequired, className, onChange }) => {
   const [showPW, setShowPW] = useState(false)
 
   const Icon = () => (
@@ -21,25 +20,19 @@ export const Input = ({ value, label, icon, type, error, name, customInput, plac
     </p>
   )
 
-  const Tip = () => (
-    <button onClick={() => Swal.fire("", tipMessage)} className="button is-rounded is-paddingless is-warning tooltip">
-      ?
-    </button>
-  )
-
   return (
     <Fragment>
       <label className="label" htmlFor={name}>
         {label}
         {isRequired && <span className="has-text-danger"> *</span>}
-        {tipMessage && <Tip />}
       </label>
       <div className={`field ${type === "password" && 'has-addons has-addons-right'}`}>
         <div className={`control ${type === "password" && 'is-expanded'} ${icon && 'has-icons-left'}`}>
           {icon && <Icon />}
           {
             customInput ? customInput : <input value={value} onChange={onChange} name={name}
-              className={`input ${className}`} type={type === "password" ? (!showPW ? "password" : "text") : type} placeholder={placeholder} />
+              className={`input ${className}`} type={type === "password" ? (!showPW ? "password" : "text") : type}
+              placeholder={placeholder} />
           }
         </div>
         {type === "password" && <EyeIcon />}
