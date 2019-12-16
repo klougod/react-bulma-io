bb3f99674327f1657eeb2cb963ba3bb820079c69
klougod <brunoomagalhaes98@gmail.com>
2019-12-16 17:09:54 -0300

    fixed type conference in Hero component

--- src/lib/components/Hero/Hero.js
+++ src/lib/components/Hero/Hero.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { Children } from 'react'
 import PropTypes from 'prop-types'
+import { HeroHead, HeroBody, HeroFoot } from './'
+
+const heroHeadType = (<HeroHead />).type
+const heroBodyType = (<HeroBody />).type
+const heroFootType = (<HeroFoot />).type
 
 export const Hero = ({ className, children }) => (
   <section className={`hero ${className}`}>
-    {children}
+    { Children.toArray(children).filter(c => c.type === heroHeadType) }
+    { Children.toArray(children).filter(c => c.type === heroBodyType) }
+    { Children.toArray(children).filter(c => c.type === heroFootType) }
   </section>
 )
 
