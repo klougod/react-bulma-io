033722ea0aa76cec870f246dc8a794e9ad78d19c
klougod <brunoomagalhaes98@gmail.com>
2019-12-16 17:09:16 -0300

    fixed type conference in Card component

--- src/lib/components/Card/Card.js
+++ src/lib/components/Card/Card.js
@@ -1,12 +1,18 @@
 import React, { Children } from 'react'
 import PropTypes from 'prop-types'
+import { CardHeader, CardImage, CardContent, CardFooter } from './'
+
+const cardHeaderType = (<CardHeader />).type
+const cardImageType = (<CardImage />).type
+const cardContentType = (<CardContent />).type
+const cardFooterType = (<CardFooter />).type
 
 export const Card = ({ className, children }) => (
   <div className={`card ${className}`}>
-    { Children.toArray(children).filter(c => c.type.name === "CardHeader") }
-    { Children.toArray(children).filter(c => c.type.name === "CardImage") }
-    { Children.toArray(children).filter(c => c.type.name === "CardContent") }
-    { Children.toArray(children).filter(c => c.type.name === "CardFooter") }
+    { Children.toArray(children).filter(c => c.type === cardHeaderType) }
+    { Children.toArray(children).filter(c => c.type === cardImageType) }
+    { Children.toArray(children).filter(c => c.type === cardContentType) }
+    { Children.toArray(children).filter(c => c.type === cardFooterType) }
   </div>
 )
 
