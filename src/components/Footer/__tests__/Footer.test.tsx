272107c96484abf3672d2dfd9863236fd3be5204
klougod <brunoomagalhaes98@gmail.com>
2020-02-14 18:10:00 -0300

    Added Footer component tests

---
+++ src/components/Footer/__tests__/Footer.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Enzyme from 'enzyme'
+
+import Footer from '../'
+
+describe('Footer component test', function () {
+  it('should render without crash without props', function () {
+    const footerMock = Enzyme.shallow(<Footer />)
+    expect(footerMock).toMatchSnapshot()
+  })
+
+  it('should render without crash with props', function () {
+    const footerMockProps = {
+      className: 'footerCLass',
+    }
+    const footerMock = Enzyme.shallow(<Footer {...footerMockProps} />)
+    expect(footerMock).toMatchSnapshot()
+  })
+})
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,7 +11,7 @@ Footer.defaultProps = {
 
 Footer.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.node.isRequired
+  children: PropTypes.node
 }
 
 export default Footer
