83a7917bfb95ba7770a8b35d3d8a98f94f7790b7
klougod <brunoomagalhaes98@gmail.com>
2020-02-26 19:57:54 -0300

    added dropdown btn tests

--- src/components/Dropdown/__tests__/Dropdown.test.tsx
+++ src/components/Dropdown/__tests__/Dropdown.test.tsx
@@ -26,4 +26,14 @@ describe('Dropdown component test', function () {
     const dropdownMock = Enzyme.shallow(<Dropdown {...dropdownMockProps} />)
     expect(dropdownMock).toMatchSnapshot()
   })
+
+  it('should call onclick function prop if dropdown is clicked', function () {
+    const dropdownMockProps = { onClick: jest.fn() }
+    const dropdownMock = Enzyme.shallow(<Dropdown {...dropdownMockProps} />)
+    dropdownMock
+      .find('div')
+      .at(0)
+      .simulate('click', { target: {} })
+    expect(dropdownMock).toMatchSnapshot()
+  })
 })
