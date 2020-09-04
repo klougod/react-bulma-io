3e587f81724a6bad114884f40e8d1ba0fa2c3b27
klougod <brunoomagalhaes98@gmail.com>
2020-09-04 19:08:26 -0300

    build: fix rollup build proccess

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   singleQuote: true,
   trailingComma: 'all',
-  arrowParens: 'avoid'
-}
+  semi: false,
+  arrowParens: 'avoid',
+};
--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,92 @@
 import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
+import commonjs from '@rollup/plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+import babel from '@rollup/plugin-babel'
+import { dirname } from 'path'
 
 import pkg from './package.json'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      exclude: ['**/__tests__/**'],
-      clean: true
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
+const getPluginsConfig = (format = 'esm', prod, mini) => {
+  let tsconfigOverride =
+    format === 'esm' ? { declaration: true, declarationDir: 'build' } : {}
+  const sortie = [
+    nodeResolve({
+      extensions: extensions,
+      jsnext: true,
+      browser: true,
+      preferBuiltins: false,
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(
+        prod ? 'production' : 'development',
+      ),
     }),
     commonjs({
-      include: ['node_modules/**']
-    })
+      include: 'node_modules/**',
+    }),
+    typescript({
+      exclude: ['**/__tests__/**'],
+      tsconfigOverride: { compilerOptions: tsconfigOverride },
+    }),
+    babel({
+      extensions: extensions,
+      include: ['src/**/*'],
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+    }),
+    external(),
+  ]
+  if (mini) {
+    sortie.push(
+      terser({
+        compress: {
+          unused: false,
+          collapse_vars: false,
+        },
+        output: {
+          comments: !prod,
+        },
+      }),
+    )
+  }
+  return sortie
+}
+
+const getOutput = (format = 'esm') => {
+  const equalOpts = {
+    format,
+    exports: 'named',
+    sourcemap: true,
+  }
+  if (format === 'esm') {
+    return {
+      file: pkg.module,
+      ...equalOpts,
+    }
+  }
+
+  // pkg is package.json
+  return { file: pkg.main, ...equalOpts }
+}
+
+export default CLIArgs => {
+  const prod = !!CLIArgs.prod
+  const mini = !!CLIArgs.mini
+  return [
+    {
+      input: 'src/index.ts',
+      output: getOutput('cjs'),
+      plugins: getPluginsConfig('cjs', prod, mini),
+    },
+    {
+      input: 'src/index.ts',
+      output: getOutput('esm'),
+      plugins: getPluginsConfig('esm', prod, mini),
+    },
   ]
 }
